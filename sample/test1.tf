65a85daeb7e6a0c9bdbcc5537d5a2a601a349d79
Sivakumar S <sivakumarit42@gmail.com>
2023-02-23 17:50:18 +0530

    changes

--- sample/test1.tf
+++ sample/test1.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "ec2" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+  vpc_security_group_ids = ["sg-0cbce48f1b39fa890"]
   tags = {
-    Name = "test"
+    Name = "test1"
   }
 }
 
