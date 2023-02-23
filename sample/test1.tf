4829b7da7c0c7740457e8dfc035a78c302dfc1ea
Sivakumar S <sivakumarit42@gmail.com>
2023-02-23 17:33:11 +0530

    changes

--- sample/test1.tf
+++ sample/test1.tf
@@ -1,2 +1,8 @@
-sample1 = "Hello Sample1 from Auto"
-
+resource "aws_instance" "ec2" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+  tags = {
+    Name = "test"
+  }
+}
