47024065eb5ced9c0dce282a21f293677792c4f7
Sivakumar S <sivakumarit42@gmail.com>
2023-02-23 19:08:56 +0530

    changes

---
+++ 03-datasource/data.tf
@@ -0,0 +1,9 @@
+data "aws_ami" "test" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = [" 973714476881"]
+
+}
+output "ami-id" {
+  value = data.aws_ami.test.image_id
+}
