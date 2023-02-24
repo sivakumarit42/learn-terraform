90fda4a5f50814c6590797e418a1d52e8baf5d3a
Sivakumar S <sivakumarit42@gmail.com>
2023-02-24 09:53:44 +0530

    changes

--- 05-variable-types/variable-types.tf
+++ 05-variable-types/variable-types.tf
@@ -20,13 +20,3 @@ output "sample5" {
   value = "first and second value of sample1 block:1 is:${var.sample5[0]},2 is:${var.sample5[1]}"
   #value1 = var.sample1[0]
 }
-variable "sample6" {
-  default =  {
-    string = "Hello",
-    number = 100,
-    boolean = true
-  }
-}
-output "sample6" {
-  value = "first and second and third value of sample1 block:1 is:${var.sample6[0]},2 is:3,${var.sample6[1]}"
-}
