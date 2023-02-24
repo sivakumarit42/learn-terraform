d44c3b64fc77e4dbb18c68ce43966d937f3f963d
Sivakumar S <sivakumarit42@gmail.com>
2023-02-24 10:02:58 +0530

    changes

--- 05-variable-types/variable-types.tf
+++ 05-variable-types/variable-types.tf
@@ -20,3 +20,17 @@ output "sample5" {
   value = "first and second value of sample1 block:1 is:${var.sample5[0]},2 is:${var.sample5[1]}"
   #value1 = var.sample1[0]
 }
+#3.Map Variable Type
+variable "sample7" {
+  default =  {
+    string = "Hello",
+    number = 100,
+    boolean = true
+  }
+}
+output "sample7" {
+  value = var.sample7.boolean
+  value1 = var.sample7.string
+  value3 = var.sample7.number
+}
+
