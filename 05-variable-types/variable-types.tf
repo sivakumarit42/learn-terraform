7f509f7a1659c25bb532a6315e402f7c1c253964
Sivakumar S <sivakumarit42@gmail.com>
2023-02-24 11:01:10 +0530

    changes

--- 05-variable-types/variable-types.tf
+++ 05-variable-types/variable-types.tf
@@ -29,9 +29,6 @@ variable "sample7" {
   }
 }
 output "sample7" {
-  value = var.sample7.boolean
-  #value1 = var.sample7.string
-  #value3 = var.sample7.number
-
+  value = "value of map is:${var.sample7[string]},${var.sample7[number]}${var.sample7[boolean]}"
 }
 
