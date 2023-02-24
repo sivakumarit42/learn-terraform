0c93f5305c4f57868dcceeb929391ddfdbc8df04
Sivakumar S <sivakumarit42@gmail.com>
2023-02-24 09:19:57 +0530

    changes

---
+++ 05-variable-types/variable-data-types.tf
@@ -0,0 +1,23 @@
+# String Data type
+variable "sample1" {
+  default = "Hello World"
+}
+output "sample1-output-is" {
+  value = var.sample1
+}
+
+# Number data type
+variable "sample2" {
+  default = 100
+}
+output "sample2_output_is" {
+  value = "the output of sample2 is- ${sample2}"
+}
+# Boolean Data type
+variable "sample3" {
+  default = true
+}
+output "sample3" {
+  value = var.sample3
+  value1 = "the output of sample3 is: ${sample3}"
+}
