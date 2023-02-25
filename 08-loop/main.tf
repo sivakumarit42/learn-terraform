616214e2e441dccb2602f752906cf1604841b6e2
Sivakumar S <sivakumarit42@gmail.com>
2023-02-25 16:46:15 +0530

    changes

--- 08-loop/main.tf
+++ 08-loop/main.tf
@@ -1,10 +1,10 @@
-#variable "d1" {
-#  default = 5
-#}
-#
-#resource "null_resource" "nothing" {
-#  count = var.d1
-#}
+variable "d1" {
+  default = 5
+}
+
+resource "null_resource" "nothing" {
+  count = var.d1
+}
 
 variable "d2" {
   default = ["apple", "banana"]
@@ -14,24 +14,24 @@ resource "null_resource" "d2" {
   count = length(var.d2)
 }
 
-#variable "d3" {
-#  default = {
-#    apple = {
-#      name  = "apple"
-#      count = 3
-#    }
-#    banana = {
-#      name  = "banana"
-#      count = 30
-#    }
-#  }
-#}
-#
-#resource "null_resource" "d3" {
-#  for_each = var.d3
-#}
-#
-#output "FRUITS" {
-#  value = [for i in var.d2 : upper(i)]
-#
-#}
+variable "d3" {
+  default = {
+    apple = {
+      name  = "apple"
+      count = 3
+    }
+    banana = {
+      name  = "banana"
+      count = 30
+    }
+  }
+}
+
+resource "null_resource" "d3" {
+  for_each = var.d3
+}
+
+output "FRUITS" {
+  value = [for i in var.d2 : upper(i)]
+
+}
