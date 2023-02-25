976a13830dfb3263f995893b3bb66c79a7ac003a
Sivakumar S <sivakumarit42@gmail.com>
2023-02-25 13:22:57 +0530

    changes

--- 06-statefile/ec2-creation-s3.tf
+++ 06-statefile/ec2-creation-s3.tf
@@ -1,14 +1,3 @@
-
-
-resource "aws_instance" "myec2" {
-  ami                    = "ami-0a017d8ceb274537d"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-0cbce48f1b39fa890"]
-  tags = {
-    Name = "demo10"
-  }
-}
-
 terraform {
   backend "s3" {
     bucket = "mys3bucketb72"
@@ -18,3 +7,14 @@ terraform {
     region = "us-east-1"
   }
 }
+
+
+resource "aws_instance" "myec2" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0cbce48f1b39fa890"]
+  tags = {
+    Name = "demo11"
+  }
+}
+
