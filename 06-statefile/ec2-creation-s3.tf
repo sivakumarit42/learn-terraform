195df13d3ba9c6750ca2d112a5044a3ba1487b47
Sivakumar S <sivakumarit42@gmail.com>
2023-02-25 12:34:32 +0530

    changes

--- 06-statefile/ec2-creation-s3.tf
+++ 06-statefile/ec2-creation-s3.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "statefile" {
+resource "aws_instance" "myec2" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0cbce48f1b39fa890"]
