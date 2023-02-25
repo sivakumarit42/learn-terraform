bc9584f13f9a741ee12304c38be7be6922b7d622
Sivakumar S <sivakumarit42@gmail.com>
2023-02-25 20:43:21 +0530

    changes

--- roboshop-setup/servers-creation3/servers.tf
+++ roboshop-setup/servers-creation3/servers.tf
@@ -4,29 +4,29 @@ data "aws_ami" "ami" {                                  #access image id by usin
   owners      = ["973714476881"]
 }
 
-#resource "aws_instance" "frontend" {
-#  #count                 = length(var.instances)        #it wont work for nested map
-#  for_each               = var.instances
-#  ami                    = data.aws_ami.ami.image_id
-#  instance_type          = each.value["type"]
-#  vpc_security_group_ids = ["sg-0cbce48f1b39fa890"]
-#  tags = {
-#   # Name=var.instances[count.index]                    #it wont work for nested map
-#    Name=each.value["name"]
-#  }
-#}
-#variable "instances" {
-#  default = {                                #map of map
-#    catalogue = {                            #catalogue and user are key:name and type are value
-#      name = "catalogue"
-#      type = "t3.micro"
-#    },
-#    user = {
-#      name = "user"
-#      type = "t2.small"
-#    }
-#  }
-#}
+resource "aws_instance" "frontend" {
+  #count                 = length(var.instances)        #it wont work for nested map
+  for_each               = var.instances
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = each.value["type"]
+  vpc_security_group_ids = ["sg-0cbce48f1b39fa890"]
+  tags = {
+   # Name=var.instances[count.index]                    #it wont work for nested map
+    Name=each.value["name"]
+  }
+}
+variable "instances" {
+  default = {                                #map of map
+    catalogue = {                            #catalogue and user are key:name and type are value
+      name = "catalogue"
+      type = "t3.micro"
+    },
+    user = {
+      name = "user"
+      type = "t2.small"
+    }
+  }
+}
 
 ##little matured code
 #resource "aws_instance" "frontend" {
@@ -51,21 +51,21 @@ data "aws_ami" "ami" {                                  #access image id by usin
 #    }
 #  ]
 #}
-
-# im-matured code
-resource "aws_instance" "frontend" {
-  count                 = length(var.name)                  #list of variables
-  ami                    = data.aws_ami.ami.image_id
-  instance_type          = var.type[count.index]
-  vpc_security_group_ids = ["sg-0cbce48f1b39fa890"]
-  tags = {
-    Name=var.name[count.index]
-  }
-}
-
-variable "name" {
-  default = [ "catalogue", "user" ]
-}
-variable "type" {
-  default = [ "t2.micro", "t3.micro" ]
-}
+#
+## im-matured code
+#resource "aws_instance" "frontend" {
+#  count                 = length(var.name)                  #list of variables
+#  ami                    = data.aws_ami.ami.image_id
+#  instance_type          = var.type[count.index]
+#  vpc_security_group_ids = ["sg-0cbce48f1b39fa890"]
+#  tags = {
+#    Name=var.name[count.index]
+#  }
+#}
+#
+#variable "name" {
+#  default = [ "catalogue", "user" ]
+#}
+#variable "type" {
+#  default = [ "t2.micro", "t3.micro" ]
+#}
