c62ee379229b1cd623bbcd992afec61550a23984
Sivakumar S <sivakumarit42@gmail.com>
2023-02-25 13:18:30 +0530

    changes

--- 06-statefile/ec2-creation-s3.tf
+++ 06-statefile/ec2-creation-s3.tf
@@ -1,4 +1,14 @@
 
+
+resource "aws_instance" "myec2" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0cbce48f1b39fa890"]
+  tags = {
+    Name = "demo10"
+  }
+}
+
 terraform {
   backend "s3" {
     bucket = "mys3bucketb72"
@@ -6,14 +16,5 @@ terraform {
     #key    = "learn-terraform/06-statefile/terraform.tfstate"    #Here the file name is our choice.file name is inside 2 folder.
     #key     ="statefile"                                          #Here the file name is our choice,we can give any name with or without folder
     region = "us-east-1"
-  }
-}
-
-resource "aws_instance" "myec2" {
-  ami                    = "ami-0a017d8ceb274537d"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-0cbce48f1b39fa890"]
-  tags = {
-    Name = "demo9"
   }
 }
