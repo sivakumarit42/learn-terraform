cf8c647f256c8258c608d4d684957f879fc08d39
Sivakumar S <sivakumarit42@gmail.com>
2023-02-25 18:25:19 +0530

    changes

--- roboshop-setup/servers-creation3/servers.tf
+++ roboshop-setup/servers-creation3/servers.tf
@@ -3,74 +3,28 @@ data "aws_ami" "ami" {
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
 }
+#access image id by using datasourse
 
-
-resource "aws_instance" "instances" {
+resource "aws_instance" "frontend" {
+  #count                 = length(var.instances)        #it wont work for nested map
   for_each               = var.instances
   ami                    = data.aws_ami.ami.image_id
-  instance_type          = each.value["type"]
-  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+  instance_type          = each.value[type]
+  vpc_security_group_ids = ["sg-0cbce48f1b39fa890"]
   tags = {
-    Name = each.value["name"]
+   # Name=var.instances[count.index]                    #it wont work for nested map
+    Name=each.value[name]
   }
 }
-
 variable "instances" {
   default = {
     catalogue = {
-      name = "catalogue"
+      name = catalogue
       type = "t3.micro"
-    }
+    },
     user = {
-      name = "user"
-      type = "t3.small"
+      name = user
+      type = "t2.small"
     }
   }
 }
-
-output "ec2" {
-  value = [for k, v in aws_instance.instances : "${k} - ${v.public_ip}"]
-}
-
-## Immature code
-//variable "names" {
-//  default = [ "catalogue", "user"]
-//}
-//
-//variable "types" {
-//  default = ["t3.micro", "t3.small"]
-//}
-//
-//resource "aws_instance" "instances" {
-//  count = length(var.names)
-//  ami                    = data.aws_ami.ami.image_id
-//  instance_type          = var.types[count.index]
-//  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
-//  tags = {
-//    Name = var.names[count.index]
-//  }
-//}
-
-## Little mature code
-//variable "demo" {
-//  default = [
-//    {
-//      name = "catalogue"
-//      type = "t3.micro"
-//    },
-//    {
-//      name = "user"
-//      type = "t3.small"
-//    }
-//  ]
-//}
-//
-//resource "aws_instance" "instances" {
-//  count                  = length(var.demo)
-//  ami                    = data.aws_ami.ami.image_id
-//  instance_type          = var.demo[count.index]["type"]
-//  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
-//  tags = {
-//    Name = var.demo[count.index]["name"]
-//  }
-//}
