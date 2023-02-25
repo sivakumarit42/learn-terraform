b8df42391e2cd9b3b9b0dbb92af11bbf4a8b021e
Sivakumar S <sivakumarit42@gmail.com>
2023-02-25 19:13:13 +0530

    changes

--- roboshop-setup/servers-creation3/servers.tf
+++ roboshop-setup/servers-creation3/servers.tf
@@ -1,30 +1,54 @@
-data "aws_ami" "ami" {
+data "aws_ami" "ami" {                                  #access image id by using datasourse
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
 }
-#access image id by using datasourse
 
+#resource "aws_instance" "frontend" {
+#  #count                 = length(var.instances)        #it wont work for nested map
+#  for_each               = var.instances
+#  ami                    = data.aws_ami.ami.image_id
+#  instance_type          = each.value["type"]
+#  vpc_security_group_ids = ["sg-0cbce48f1b39fa890"]
+#  tags = {
+#   # Name=var.instances[count.index]                    #it wont work for nested map
+#    Name=each.value["name"]
+#  }
+#}
+#variable "instances" {
+#  default = {                                #map of map
+#    catalogue = {                            #catalogue and user are key:name and type are value
+#      name = "catalogue"
+#      type = "t3.micro"
+#    },
+#    user = {
+#      name = "user"
+#      type = "t2.small"
+#    }
+#  }
+#}
+
+#little im-matured code
 resource "aws_instance" "frontend" {
-  #count                 = length(var.instances)        #it wont work for nested map
-  for_each               = var.instances
+  count                 = length(var.instances)                  #list of map
   ami                    = data.aws_ami.ami.image_id
-  instance_type          = each.value["type"]
+  instance_type          = var.instances[count.index]["type"]
   vpc_security_group_ids = ["sg-0cbce48f1b39fa890"]
   tags = {
-   # Name=var.instances[count.index]                    #it wont work for nested map
-    Name=each.value["name"]
+     Name=var.instances[count.index]["name"]
   }
 }
+
 variable "instances" {
-  default = {
-    catalogue = {                            #catalogue and user are key:name and type are value
+  default = [
+    {
       name = "catalogue"
-      type = "t3.micro"
+      type = "t2.small"
     },
-    user = {
+    {
       name = "user"
       type = "t2.small"
     }
-  }
+  ]
 }
+
