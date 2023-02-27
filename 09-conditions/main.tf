e6fa648463e2198e6420963ec157fea159d8138d
Sivakumar S <sivakumarit42@gmail.com>
2023-02-27 11:15:23 +0530

    changes

--- 09-conditions/main.tf
+++ 09-conditions/main.tf
@@ -11,7 +11,7 @@ variable "instance_type" {}
 variable "create_instance" {}
 
 resource "aws_instance" "ec2_instance_new" {
-  count         = tobool(var.create_instance) ? 2 : 3
+  count         = tobool(var.create_instance) ? 2 : 0
   ami           = "ami-0a017d8ceb274537d"
   instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
   tags = {
