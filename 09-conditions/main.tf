7caae144ac56098a8f83db109b4dbcfee83ba455
Sivakumar S <sivakumarit42@gmail.com>
2023-02-27 10:48:51 +0530

    changes

--- 09-conditions/main.tf
+++ 09-conditions/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2_instance" {
   ami           = "ami-0a017d8ceb274537d"
-  instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
+  instance_type = var.instance_type == "" ? "t2.micro" : var.instance_type
 }
 
 variable "instance_type" {}
