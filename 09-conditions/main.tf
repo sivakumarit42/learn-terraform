2ea511eeef5239f01658d14b2cf13201958c01a7
Sivakumar S <sivakumarit42@gmail.com>
2023-02-27 10:53:36 +0530

    changes

--- 09-conditions/main.tf
+++ 09-conditions/main.tf
@@ -5,10 +5,10 @@ resource "aws_instance" "ec2_instance" {
 
 variable "instance_type" {}
 
-#variable "create_instance" {}
-#
-#resource "aws_instance" "ec2_instance_new" {
-#  count         = tobool(var.create_instance) ? 1 : 0
-#  ami           = "ami-0a017d8ceb274537d"
-#  instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
-#}
+variable "create_instance" {}
+
+resource "aws_instance" "ec2_instance_new" {
+  count         = tobool(var.create_instance) ? 1 : 0
+  ami           = "ami-0a017d8ceb274537d"
+  instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
+}
