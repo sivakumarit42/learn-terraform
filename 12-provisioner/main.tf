7b33930ef572899b9e9e7e553558489cea9268ac
Sivakumar S <sivakumarit42@gmail.com>
2023-02-28 15:31:54 +0530

    changes

--- 12-provisioner/main.tf
+++ 12-provisioner/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "ec2" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+  vpc_security_group_ids = ["sg-0cbce48f1b39fa890"]
   tags = {
-    Name = "demo"
+    Name = "test1"
   }
 
   provisioner "remote-exec" {
@@ -18,4 +18,6 @@ resource "aws_instance" "ec2" {
       "echo Hello"
     ]
   }
+
 }
+
