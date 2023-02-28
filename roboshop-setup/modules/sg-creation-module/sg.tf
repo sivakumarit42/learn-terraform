657f586b4aee86027b4e70ecf7e4d349e2bf0f78
Sivakumar S <sivakumarit42@gmail.com>
2023-02-28 16:26:43 +0530

    changes

--- roboshop-setup/modules/sg-creation-module/sg.tf
+++ roboshop-setup/modules/sg-creation-module/sg.tf
@@ -3,7 +3,7 @@ resource "aws_security_group" "allow_tls" {
   description = "Allow TLS inbound traffic"
 
   ingress {
-    description      = "TLS from VPC"
+    description      = "All"
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
@@ -15,7 +15,6 @@ resource "aws_security_group" "allow_tls" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
