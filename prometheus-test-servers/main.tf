49dc4ea724f0f215cfc9ac1595a254a194d868c0
Sivakumar S <sivakumarit42@gmail.com>
2023-03-08 11:37:51 +0530

    changes

---
+++ prometheus-test-servers/main.tf
@@ -0,0 +1,33 @@
+resource "aws_spot_instance_request" "node1" {
+  ami                    = "ami-048ce8b26a6a9b9af"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0cbce48f1b39fa890"]
+  wait_for_fulfillment   = true
+
+  tags = {
+    Name = "prom-test-server"
+  }
+}
+
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-server"
+}
+
+resource "aws_spot_instance_request" "node2" {
+  ami                    = "ami-048ce8b26a6a9b9af"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0cbce48f1b39fa890"]
+  wait_for_fulfillment   = true
+
+  tags = {
+    Name = "prom-test-node"
+  }
+}
+
+resource "aws_ec2_tag" "node2" {
+  resource_id = aws_spot_instance_request.node2.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-node"
+}
